0c27b6a0334e25f3f55157b24f386515eec76c79
thedoctorfunk <61288822+thedoctorfunk@users.noreply.github.com>
2020-02-29 17:18:20 +0100

    Animated Backggorund
    +Added animated background image to the login screen

--- uni_grade/lib/LoginScreen.dart
+++ uni_grade/lib/LoginScreen.dart
@@ -7,13 +7,15 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:uni_grade/Gecmis.dart';
 import 'package:uni_grade/GecmisLogin.dart';
 
-
 class LoginScreenPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _LoginScreenPage();
 }
 
-class _LoginScreenPage extends State<LoginScreenPage> {
+class _LoginScreenPage extends State<LoginScreenPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation<double> _backgroundAnimation;
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   final GoogleSignIn _googlSignIn = new GoogleSignIn();
 
@@ -62,107 +64,130 @@ class _LoginScreenPage extends State<LoginScreenPage> {
 
   void initState() {
     super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 20));
+
+    _backgroundAnimation =
+        CurvedAnimation(parent: _animationController, curve: Curves.linear)
+          ..addListener(() {
+            setState(() {});
+          })
+          ..addStatusListener((animationStatus) {
+            if (animationStatus == AnimationStatus.completed) {
+              _animationController.reset();
+              _animationController.forward();
+            }
+          });
+
+    _animationController.forward();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          //GoogleButton
-          Container(
-            margin: EdgeInsets.all(5),
-            width: 250,
-            height: 50,
-            child: FloatingActionButton(
-              heroTag: 'btn1',
-              onPressed: () {
-                setState(() {
-                  Navigator.pushNamed(context, "/DRP");
-                });
-              },
-              child: Row(
-                children: <Widget>[
-                  SizedBox(width: 10),
-                  Icon(FontAwesomeIcons.google, color: Colors.red),
-                  SizedBox(
-                    width: 15,
-                  ),
-                  Text('Google ile giriş yapın',style: TextStyle(fontSize: 15))
-                ],
-              ),
-              tooltip: 'Google Girişi',
-              backgroundColor: Colors.white,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(16.0))),
-            ),
-          ),
-
-          //FacebookButton
-          Container(
-            margin: EdgeInsets.all(5),
-            width: 250,
-            height: 50,
-            child: FloatingActionButton(
-              heroTag: 'btn2',
-              onPressed: () {
-                _signIn(context)
-                    .then((FirebaseUser user) => print(user))
-                    .catchError((e) => print(e));
-              },
-              child: Row(
-                children: <Widget>[
-                  SizedBox(width: 10),
-                  Icon(FontAwesomeIcons.facebook, color: Colors.white),
-                  SizedBox(
-                    width: 15,
-                  ),
-                  Text(
-                    'Facebook ile giriş yapın',
-                    style: TextStyle(color: Colors.white, fontSize: 15),
-                  ),
-                ],
-              ),
-              tooltip: 'Facebook Girişi',
-              backgroundColor: Colors.blue,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(16.0))),
-            ),
-          ),
-
-          //MisafirButton
-          new GestureDetector(
-            child: Container(
+        body: Stack(
+      children: <Widget>[
+        Image.asset(
+          'lib/resources/images/background.jpg',
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+          alignment: FractionalOffset(_backgroundAnimation.value, 0),
+        ),
+        Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //GoogleButton
+            Container(
               margin: EdgeInsets.all(5),
               width: 250,
               height: 50,
               child: FloatingActionButton(
-                heroTag: 'btn3',
+                heroTag: 'btn1',
                 onPressed: () {
                   setState(() {
-                    Navigator.pushNamed(context, "/GecmisLogin");
+                    Navigator.pushNamed(context, "/DRP");
                   });
                 },
-                child: Text(
-                  'Misafir Olarak Devam Edin',
-                  style: TextStyle(color: Colors.white),
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(width: 10),
+                    Icon(FontAwesomeIcons.google, color: Colors.red),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text('Google ile giriş yapın',
+                        style: TextStyle(fontSize: 15))
+                  ],
                 ),
-                tooltip: 'Misafir Girişi',
-                backgroundColor: Colors.brown,
+                tooltip: 'Google Girişi',
+                backgroundColor: Colors.white,
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(16.0))),
               ),
             ),
-          )
-        ],
-      )),
-    );
+
+            //FacebookButton
+            Container(
+              margin: EdgeInsets.all(5),
+              width: 250,
+              height: 50,
+              child: FloatingActionButton(
+                heroTag: 'btn2',
+                onPressed: () {
+                  _signIn(context)
+                      .then((FirebaseUser user) => print(user))
+                      .catchError((e) => print(e));
+                },
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(width: 10),
+                    Icon(FontAwesomeIcons.facebook, color: Colors.white),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text(
+                      'Facebook ile giriş yapın',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                  ],
+                ),
+                tooltip: 'Facebook Girişi',
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(16.0))),
+              ),
+            ),
+
+            //MisafirButton
+            new GestureDetector(
+              child: Container(
+                margin: EdgeInsets.all(5),
+                width: 250,
+                height: 50,
+                child: FloatingActionButton(
+                  heroTag: 'btn3',
+                  onPressed: () {
+                    setState(() {
+                      Navigator.pushNamed(context, "/GecmisLogin");
+                    });
+                  },
+                  child: Text(
+                    'Misafir Olarak Devam Edin',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  tooltip: 'Misafir Girişi',
+                  backgroundColor: Colors.brown,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(16.0))),
+                ),
+              ),
+            )
+          ],
+        )),
+      ],
+    ));
   }
 }
 
